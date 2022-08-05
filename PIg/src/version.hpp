df717099507ff7d2239e8a14a49ba1cf915ac0a7
bear <bear@prosoftsystems.ru>
2022-08-05 19:48:29 +0500

    Изменить версию на 01.53 для возможности работы с новым конфигуратором
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- PIg/src/version.hpp
+++ PIg/src/version.hpp
@@ -3,7 +3,7 @@
 
 #define PROJECT_NAME "PIg"
 #define PROJECT_VER  ""
-#define PROJECT_VER_MAJOR 0xA1
+#define PROJECT_VER_MAJOR 0x1
 #define PROJECT_VER_MINOR 0x53
 #define PTOJECT_VER_PATCH 0x00
 
